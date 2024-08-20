2cd2a722ae3689de17a81c57fd9e3cd938f46e30
shinobiKoda <sirp2804@gmail.com>
2024-08-20 13:04:47 +0100

    completed desktop nav

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const nav_links = document.querySelector(".nav-links");
 const title_links = document.querySelectorAll(".link-title");
+const desktop_nav_links = document.querySelectorAll(
+  ".desktop-sub-link-heading"
+);
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -27,5 +30,27 @@ title_links.forEach((title) => {
     // Toggle the clicked sub-link and arrow
     sub_link.classList.toggle("show");
     arrow_img.classList.toggle("rotate");
+  });
+});
+
+desktop_nav_links.forEach((link) => {
+  link.addEventListener("click", () => {
+    const sub_link = link.nextElementSibling;
+    const arrow_img = link.querySelector(".desktop-arrow-img");
+
+    //Hide all sub links and reset all arrows
+    document.querySelectorAll(".desktop-nav-sub-link").forEach((link) => {
+      if (link != sub_link) {
+        link.classList.remove("show-desktop");
+      }
+    });
+    document.querySelectorAll(".desktop-arrow-img").forEach((img) => {
+      if (img != arrow_img) {
+        img.classList.remove("desktop-rotate");
+      }
+    });
+
+    sub_link.classList.toggle("show-desktop");
+    arrow_img.classList.toggle("desktop-rotate");
   });
 });
